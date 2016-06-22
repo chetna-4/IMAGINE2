077b578397483c64ee8ef8c1c84098f53bf2c575
Chetna <chetna4395@gmail.com>
2016-06-22 17:02:50 +0530

    commiting now

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Variable()
+        {
+            var variables = new List<Variable>();
+            variables.Add(new Variable { Id = 1, Name = "Variable1", DataType = "Int" });
+            variables.Add(new Variable { Id = 2, Name = "Variable2", DataType = "String" });
+            variables.Add(new Variable { Id = 3, Name = "Variable3", DataType = "Int" });
+
+            return View(variables);
+        }
+    }
+}
---
+++ WebApplication2/Models/variables1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class variables
+    {
+        public List<Variable> variableList = new List<Variable>();
+        public int count = 1;
+
+        public Variable Getuser(int id)
+        {
+            Variable varmod = null;
+            foreach (Variable vm in variableList)
+                if (vm.Id== id)
+                    varmod = vm;
+            return varmod;
+        }
+
+        public void CreateVar(Variable varmod)
+        {
+
+            varmod.Id = count;
+            count++;
+            varmod.Name = "String";
+            varmod.DataType = "String";
+            variableList.Add(varmod);
+        }
+    }
+}
